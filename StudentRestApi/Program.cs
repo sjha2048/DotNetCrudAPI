419490d5007832f851fcbc144ed68d6bf7ef8336
Sahil S <sahil.s@hanu.com>
2021-04-01 16:03:34 +0530

    added dockerfile

--- StudentRestApi/Program.cs
+++ StudentRestApi/Program.cs
@@ -20,6 +20,7 @@ namespace StudentRestApi
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*:80");
                     webBuilder.UseStartup<Startup>();
                 });
     }
